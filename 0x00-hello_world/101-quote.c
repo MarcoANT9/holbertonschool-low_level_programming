b03b4fb1bb3c0532e7389b54bce11ca05b5ae7a2
MarcoANT9 <acalde27@hotmail.com>
2019-09-18 21:50:03 +0000

    9.Quote First commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints a text to the standard error
+ *
+ *
+ * Description - Prints "and that piece of art is usefull" from Dora Korpar
+ * using the "Write" command.
+ *
+ *
+ *
+ * Return: Always fails (1)
+ *
+ */
+
+
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar,\
+2015-10-19\n", 59);
+
+
+	return (1);
+
+}
