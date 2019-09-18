5c3c0b79e8b49e539f5d4f84469249eb480e68ae
MarcoANT9 <acalde27@hotmail.com>
2019-09-18 19:10:55 +0000

    4.Puts 4th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,16 +8,14 @@
  * building a multilingual puzzle" followed by a new line
  *
  * Return: - Always 0 (Success)
+ *
  */
 
 int main(void)
 {
 
-	char string[] = "Programming is like building a multilingual puzzle";
-
-	puts(string);
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
-
 
 }
