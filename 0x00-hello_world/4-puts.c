77a1062038cddfd4d5b8959fbf8055a3f3477680
MarcoANT9 <acalde27@hotmail.com>
2019-09-18 18:58:06 +0000

    4.Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Print a string text
+ *
+ * Description: This program will always print the string "Programming is like
+ * building a multilingual puzzle" followed by a new line
+ *
+ * Return: - Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char string[50];
+
+	strcpy(str, "Programming is like building a multilingual puzzle");
+	puts(string);
+	return (0);
+
+}
