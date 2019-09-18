2c0a83373ca5133abc42ee58b2d158025a50f4b9
MarcoANT9 <acalde27@hotmail.com>
2019-09-18 19:25:59 +0000

    5.Printf First commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string text using printf
+ *
+ * Description: This program will always print the string "with proper grammar,
+ * but the outcome is a piece of art," followed by a new line
+ *
+ * Return: - Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	char str []="with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s",str);
+
+	return (0);
+
+}
