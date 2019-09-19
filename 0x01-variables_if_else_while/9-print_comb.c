99f01b1c789cfbc4cc9ed884926e5497130e6b80
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 19:41:23 +0000

    9.Combinations First commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Base 16
+ * main - Print Combination
  *
- * Description - This program will print all numbers of base 16.
+ * Description - Prints all combinations of single-digit numbers.
  *
  * Return: Always Success (0)
  *
@@ -21,14 +21,13 @@ int main(void)
 
 		putchar(i);
 
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+
+		}
 	}
-
-	for (i = 97; i < 103; i++)
-        {
-
-		putchar(i);
-
-        }
 
 
 	putchar('\n');
