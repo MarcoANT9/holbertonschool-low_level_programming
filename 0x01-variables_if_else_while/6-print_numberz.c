def0e8eefcde4c744d436e1b96875b2792786913
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 19:23:44 +0000

    6.Numberz First commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet
+ *
+ * Description - This function prints all the alphabet it lowercase.
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+
+int main(void)
+{
+
+	int i;
+
+
+	for (i =48; i < 58; i++)
+	{
+
+		putchar(i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
