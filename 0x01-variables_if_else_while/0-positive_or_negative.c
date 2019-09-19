dbd7e65c0f2a8a4c402eae40e9c810025fbbf80b
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 16:02:19 +0000

    0.Postive-negative First Commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - gives n a random value
+ *
+ * Description - Gives the var n a random value, then it determines if the
+ * value is greather, lower or equal to 0.
+ *
+ * Return: Always succesfull (0) / prints the sign of the number n.
+ *
+ */
+
+int main(void)
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+
+		printf("%d is negative\n", n);
+
+	}
+
+	else if (n > 0)
+
+	{
+
+		printf("%d is positive\n", n);
+
+	}
+
+	else
+
+	{
+
+		printf("%d is zero\n", n);
+
+	}
+
+	return (0);
+
+
+}
