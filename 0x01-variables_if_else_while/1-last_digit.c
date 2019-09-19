9ccca63029359360a59a4972fb4d91c58c27f9ec
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 18:32:47 +0000

    2. Alphabet 2nd commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,10 +28,10 @@ int main(void)
 	if (last > 5)
 	{
 
-		printf("Last digit of %d is %d and is great \
-er than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater \
+than 5\n", n, last);
 	}
-
+	
 	else if (last == 0)
 	{
 
