e5a73828ae396382591b530ba7ce7ab828d6deb2
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 19:35:25 +0000

    8.Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Base 16
+ *
+ * Description - This program will print all numbers of base 16.
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+
+int main(void)
+{
+
+	int i;
+
+
+	for (i = 48; i < 58; i++)
+	{
+
+		putchar(i);
+
+	}
+
+	for (i = 97; i < 103; i++)
+	{
+
+		putchar(i);
+
+	}
+
+
+	putchar('\n');
+
+	return (0);
+
+}
