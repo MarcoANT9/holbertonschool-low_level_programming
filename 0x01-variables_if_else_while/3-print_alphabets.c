a651835ac83c58be0b7d85502ea632879f1ffea3
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 18:47:21 +0000

    3. Print Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet
+ *
+ * Description - This function prints all the alphabet it lowercase.
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+
+int main(void)
+{
+
+	char a[100] = "abcdefghijklmnopqrstuvwxyz";
+	char A[100] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+	int i;
+
+
+	for (i = 0; i < 26; i++)
+	{
+
+		putchar(a[i]);
+
+	}
+
+	for (i =0; i < 26; i++)
+	{
+
+		putchar(A[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c~
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet
+ *
+ * Description - This function prints all the alphabet it lowercase.
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+
+int main(void)
+{
+
+	char a[100] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+
+	for (i = 0; i < 26; i++)
+	{
+
+		putchar(a[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
