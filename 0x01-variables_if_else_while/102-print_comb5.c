6d22a9c2bcfdf1259ce081184f684cd025a023ef
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 21:27:46 +0000

    102.Combinatios 5 First commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * main - Print Combination V.2
+ *
+ * Description - Prints all combinations of double-digit numbers.
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	i = 48;
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 57)
+		{
+			k = i;
+			while (k < 58)
+			{
+				l = j + 1;
+				while (l < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					if (i != 57 || j != 56 || k != 57 || l
+!= 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+					l += 1;
+				}
+				k += 1;
+			}
+			j += 1;
+			l += 1;
+		}
+		i += 1;
+	}
+	putchar('\n');
+	return (0);
+}
