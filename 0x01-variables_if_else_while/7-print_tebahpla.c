a751ecd8cbad08547b50e9af63fe0a76f44fd95f
MarcoANT9 <acalde27@hotmail.com>
2019-09-19 19:28:58 +0000

    7.Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet
+ *
+ * Description - This function prints all the alphabet it lowercase.
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+
+int main(void)
+{
+
+	int i;
+
+
+	for (i = 122; i > 96; i--)
+	{
+
+		putchar(i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
