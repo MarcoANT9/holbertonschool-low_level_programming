0cd67ce1e01e026644c02a02a57ea5d694d1a891
MarcoANT9 <acalde27@hotmail.com>
2019-09-24 14:25:40 +0000

    9.Times table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,1 +1,14 @@
-#include
+#include "holberton.h"
+
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+ * times_table - Prints the 9 times table starting with 0.
+ *
+ *
+ *
+ * Return: Nothing (void)
+ *
+ *
+ *
+ */
+
+
+
+void times_table(void)
+{
+
+	int i;
+	int j;
+	int res;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (j = 0; j < 10; j++)
+		{
+
+			res = i * j;
+
+			if (j != 0)
+			{
+				_putchar(' ');
+				if ((res / 10) == 0)
+				{
+					_putchar(' ');
+				}
+			}
+			if ((res / 10) == 0)
+			{
+				_putchar(res + '0');
+			}
+
+			else
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+
+			}
+
+			_putchar(',');
+
+		}
+		_putchar ('\n');
+	}
+}
