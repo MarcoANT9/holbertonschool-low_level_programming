3458e94f7d76aa2b0d6d303ff45bec25f9f4951e
MarcoANT9 <acalde27@hotmail.com>
2019-09-24 14:39:55 +0000

    10.Add First commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers and returns the result
+ *
+ * @i: First number to sum
+ * @j: Second number to sum
+ *
+ * Return: The sum of the numbers (i+j)
+ *
+ */
+
+int add (int i, int j)
+{
+	int res;
+
+	res = i + j;
+
+	return (res);
+
+}
