55dba8552ba07d7a7355d2a011310b0e70cbc752
MarcoANT9 <acalde27@hotmail.com>
2019-09-25 18:56:01 +0000

    103.Fibonacci2 First commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - writes the sum og the even-valued terms of the fibonacci sequence
+ * only considers the terms below 4'000.000
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int f1 = 1, f2 = 2, sumf, sump;
+
+	sumf = f1 + f2;
+
+	sump = f2;
+	while (sumf < 4000000)
+	{
+		sumf = f1 + f2;
+		f1 = f2;
+		f2 = sumf;
+
+		if (sumf % 2 == 0)
+		{
+			sump = sump + sumf;
+		}
+
+	}
+
+	printf("%lu\n", sump);
+
+
+	return (0);
+}
