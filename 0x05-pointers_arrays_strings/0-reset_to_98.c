45df0c6385b7c5ca6255b7f6131da2efacbb3fe8
MarcoANT9 <acalde27@hotmail.com>
2019-10-01 14:00:04 +0000

    First Commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Assigns whatever value the input variable has to 98
+ *
+ * @n: Input pointer
+ *
+ *
+ * Return: Nothing (void)
+ *
+ */
+
+
+
+
+void reset_to_98(int *n)
+{
+	int h = 98;
+
+	*n = h;
+
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,22 @@
+/**  This is the Holberton libraty */
+
+void reset_to_98(int *n); /** Resets any value of n to 98 */
+
+void swap_int(int *a, int *b); /** Swaps two numbers */
+
+int _strlen(char *s); /** Returns the length of a string */
+
+void _puts(char *str); /** Prints a string in the std ouput */
+
+void print_rev(char *s); /** Prints a string in reverse */
+
+void rev_string(char *s); /** Reverses a string */
+
+void puts2(char *str); /** Prints every character of a string */
+
+void puts_half(char *str); /** Prints half of a string */
+
+void print_array(int *a, int n); /** Prints an array */
+
+char *_strcpy(char *dest, char *src); /** Copies the string pointed to src */
+
