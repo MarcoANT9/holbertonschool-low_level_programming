6beaa456a8cbb9b3b50d21b91210949ee20398b1
MarcoANT9 <acalde27@hotmail.com>
2019-10-01 19:11:38 +0000

    7.PutsHalf First commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * puts_half - Prints half a string starts with the middle
+ *
+ * @s: The string to be printed
+ *
+ * Return: Nothing (void)
+ */
+
+
+
+void puts_half(char *s)
+{
+	int i = 0;
+	char a = *s;
+
+	while (a != '\0')
+	{
+		i = i + 1;
+		a = *(s + i);
+	}
+
+	if (i % 2 != 0)
+	{
+		i = i - 1;
+	}
+
+	i = i / 2;
+	a = *(s + i);
+
+	while (a != '\0')
+	{
+		if (a != '\n')
+		{
+			_putchar(a);
+		}
+		i = i + 1;
+		a = *(s + i);
+	}
+	_putchar('\n');
+}
