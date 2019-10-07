f34b909d2854d85d5c9b7580c68a028a0a07fd7c
MarcoANT9 <acalde27@hotmail.com>
2019-10-07 18:39:22 +0000

    2.StrChr First Commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: Pointer to the string
+ * @c: Character to find
+ *
+ * Description - Search the first iteration of a character in a string, then
+ *               returns a pointer to the location of said character.
+ *
+ * Return: → char pointer (s + char_pos) if char exist in string.
+ *         → NULL if character does not exist in the string.
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+
+		i++;
+	}
+
+	return ('\0');
+
+}
