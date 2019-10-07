a7d594eb8b11e1039b35de03f7b918727e142377
MarcoANT9 <acalde27@hotmail.com>
2019-10-07 18:46:17 +0000

    2.StrChr Second Commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,14 +18,16 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	while (*(s + i) != '\0')
+	while (*(s) != '\0')
 	{
-		if (*(s + i) == c)
+		if (*(s) == c)
 		{
 			return (s + i);
 		}
 
+		s++;
 		i++;
+
 	}
 
 	return ('\0');
