a07d81e95ac59687c25585338ee0e9323f68f837
MarcoANT9 <acalde27@hotmail.com>
2019-10-08 02:20:23 +0000

    4.StrPbrk First Commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ *
+ * @s: String to search
+ * @accept: Acceptable bytes
+ *
+ *
+ *
+ * Return: Pointer to matching byte
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+
+		while (*(accept + j) != '\0')
+		{
+
+			if (*(s + i) == (*(accept + j)))
+			{
+				return (s + i);
+			}
+
+			j++;
+		}
+
+		i++;
+
+	}
+
+	return (s + i);
+
+}
