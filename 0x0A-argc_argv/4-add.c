9c0820d2487edc55f91fa079c2f512f55cef2b32
MarcoANT9 <acalde27@hotmail.com>
2019-10-12 19:57:37 +0000

    4.Add 4th commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -36,7 +36,8 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 
-			if (*argv[i] >= 48 && *argv[i] <= 57)
+			if ((*argv[i] >= 48 && *argv[i] <= 57) || *argv[i] == 45
+)
 			{
 				sum = sum + atoi(argv[i]);
 			}
