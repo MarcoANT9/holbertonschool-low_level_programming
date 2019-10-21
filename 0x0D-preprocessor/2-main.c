3e2006207f2d09f5e5e766c9f6d51b648dde0fc5
MarcoANT9 <acalde27@hotmail.com>
2019-10-21 18:37:55 +0000

    2.File Name First Commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+#define MAIN_NAME __FILE__
+
+
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ *
+ *
+ *
+ *
+ * Return: Always successfull (0)
+ *
+ */
+
+int main(void)
+{
+
+	printf(MAIN_NAME "\n");
+
+
+	return (0);
+
+}
