1f236e135073f9d591c859ac2cf11eff237403d7
MarcoANT9 <acalde27@hotmail.com>
2019-10-21 20:48:59 +0000

    Dog Structure 2nd commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,18 @@
 #define DOG_H
 
 
+/**
+ * struct dog - Contains data for a doge.
+ *
+ * @name: Name of the doge.
+ * @age: How old is the good boy/girl.
+ * @owner: Who owns the doge.
+ *
+ * Description - This structure contains relevant information about a dog.
+ *
+ */
+
+
 struct dog
 {
 	char *name;
