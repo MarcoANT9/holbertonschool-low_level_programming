36a708e486056e031a56d2c1a0f9691cd64e4029
MarcoANT9 <acalde27@hotmail.com>
2019-10-31 19:22:15 +0000

    2.AddNode First Commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node - Inserts a node at the beginning of a list.
+ *
+ * @h: head: Position of the head of the list.
+ * @str: String to add at the beginning of the list.
+ *
+ * Description - This program takes a string and adds it at the beginning
+ *               of the linked list.
+ *
+ * Return: Address of the new element or NULL if failed.
+ *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+	char *new_str = strdup(str);
+	int index = 0;
+
+	list_t *added_node = malloc (sizeof(list_t));
+	if (added_node == NULL)
+		return (NULL);
+
+	while (new_str[index] != '\0')
+		index++;
+
+	added_node->len = index;
+	added_node->str = new_str;
+	added_node->next = *head;
+
+	*head = added_node;
+
+	return (*head);
+
+}
