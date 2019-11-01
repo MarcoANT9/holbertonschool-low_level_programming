507e1a9868d1b0be0bb6d9d5773b5f00de76cec9
MarcoANT9 <acalde27@hotmail.com>
2019-11-01 15:51:55 +0000

    4.FreeList First Commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -14,28 +14,16 @@
  */
 void free_list(list_t *head)
 {
+	list_t *copi;
 
-	int index = 0;
-	list_t *added_node = malloc(sizeof(list_t));
-	list_t *previous = *head
+	if (head == NULL)
+		return;
 
-
-	if (added_node == NULL)
-		return (NULL);
-
-	while (str[index] != '\0')
-		index++;
-
-	added_node->str = strdup(str);
-	added_node->len = index;
-	added_node->next = NULL;
-
-
-	while ((previous->next)->next != NULL)
-	        free(previous->next);
-
-	previous->next = added_node;
-
-	return (previous);
-
+	while (head != NULL)
+	{
+		copi = head->next;
+		free(head->str);
+		free(head);
+		head = copi;
+	}
 }
