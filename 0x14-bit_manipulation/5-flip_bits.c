2b18229eab12470f9ac0434e9ea90a865579eeb5
MarcoANT9 <acalde27@hotmail.com>
2019-11-11 00:43:37 +0000

    5.Flip Bits First Commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - Counts the number of bits that need to be changed to get
+ *             from one number to the other.
+ *
+ * @n: First Number to check (beginning).
+ * @m: Second number to check (goal).
+ *
+ * Description - This program takes two unsigned integer and compares them
+ *               to determine the number of bits that need to be changed to
+ *               get from one number to another.
+ *
+ * Return: Number of bits that need to be changed.
+ */
+
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int index = 0;
+	int n_mask, m_mask;
+	unsigned int counter = 0;
+	unsigned int mask = 1;
+
+
+	while (index < 32)
+	{
+		n_mask = n & mask;
+		m_mask = m & mask;
+
+		if (n_mask != m_mask)
+			counter++;
+
+		mask = mask << 1;
+		index++;
+
+	}
+	return (counter);
+
+
+}
