bbf9950bb9fc590535068ca89d82788bb63f8a62
MarcoANT9 <acalde27@hotmail.com>
2019-11-12 22:00:41 +0000

    1.CreateFile 5th Commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	while (*(text_content + _strlen) != '\0')
 		_strlen++;
 
-	new_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
 
 	if (new_file < 0)
 		return (-1);
@@ -34,16 +35,13 @@ int create_file(const char *filename, char *text_content)
 		wrt = write(new_file, text_content, _strlen);
 		if (wrt < 0)
 			return (-1);
-		text_content++;
 	}
 	else
 	{
-		wrt = write(new_file, "", _strlen);
-		if (wrt < 0)
-			return (-1);
-		text_content++;
+		return (1);
 
 	}
+	close(new_file);
 	return (1);
 
 }
