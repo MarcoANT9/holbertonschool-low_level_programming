0ec9d7dd1157a819e20ae4dbcbd2ae317585ce7e
MarcoANT9 <acalde27@hotmail.com>
2019-11-12 22:05:59 +0000

    1.CreateFile 8th Commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	while (*(text_content + _strlen) != '\0')
 		_strlen++;
 
-	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	new_file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (new_file < 0)
 		return (-1);
