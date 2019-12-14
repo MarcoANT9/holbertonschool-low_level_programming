665ea0ac1d3f7f6b5c055045f650a0ae9cd5da3d
MarcoANT9 <acalde27@hotmail.com>
2019-12-14 17:30:35 +0000

    Return to previous

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current_node, *previous_node, *new_node;
 
-	if (h == NULL || idx < 0)
+	if (h == NULL)
 		return (NULL);
 
 
