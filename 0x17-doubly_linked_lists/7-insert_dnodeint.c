e952003d378569dac2694e57252a8f8558e0c64a
MarcoANT9 <acalde27@hotmail.com>
2019-12-15 00:53:40 +0000

    Null head correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,9 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current_node, *new_node;
 
-	if (h == NULL)
-		return (NULL);
-	if (idx == 0)
+	if (h == NULL || idx == 0)
 	{
 		new_node = add_dnodeint(h, n);
 		if (new_node == NULL)
