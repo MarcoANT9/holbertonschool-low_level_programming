c6f5c0d0319f0115b4c8adb3093ffb8a8eedfbff
MarcoANT9 <acalde27@hotmail.com>
2020-01-18 20:26:35 +0000

    Free table correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,33 @@
 #include "hash_tables.h"
 
 /**
+ * free_table - Frees a table if something goes wrong.
+ *
+ * @array: Start of the list.
+ *
+ *
+ * Description - Frees a hash table if something goes wrong.
+ *
+ * Return: Nothing (void)
+ *
+ */
+
+void free_table(hash_node_t **array)
+{
+	hash_node_t *current;
+	hash_node_t *tmp;
+
+	current = *array;
+
+	while (current->next != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+	free(current);
+}
+/**
  * hash_table_create - Creates an empty hash table
  *
  * @size: The size of the hash table.
@@ -19,7 +46,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_node_t *new_node;
+	unsigned long int index = 0;
+	hash_node_t *new_node, *next_node;
 	hash_table_t *head_table;
 
 	head_table = malloc(sizeof(hash_table_t));
@@ -39,6 +67,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_node->value = NULL;
 		new_node->next = NULL;
 	}
+	else
+		return (head_table);
+	for (index = 0; index < size; index++)
+	{
+		next_node = malloc(sizeof(hash_node_t));
+		if (next_node == NULL)
+		{
+			free_table(head_table->array);
+			free(head_table);
+			return (NULL);
+		}
+		new_node->next = next_node;
+		next_node->key = "";
+		next_node->value = "";
+		next_node->next = NULL;
+		new_node = next_node;
+	}
 	return (head_table);
-
 }
