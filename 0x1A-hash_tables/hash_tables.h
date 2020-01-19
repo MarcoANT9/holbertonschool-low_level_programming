8889aa3275b86d381bc7014aa247be145d247058
MarcoANT9 <acalde27@hotmail.com>
2020-01-19 22:11:07 +0000

    Actialization

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -55,10 +55,17 @@ unsigned long int hash_djb2(const unsigned char *str);
 /** 2.key->index */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
+/** 3.>>>ht['betty'] = 'holberton' */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
+/**4. >>> ht['betty'] */
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
+/** 5. >>> print(ht) */
+void hash_table_print(const hash_table_t *ht);
 
-
+/** 6. >>> del ht */
+void hash_table_delete(hash_table_t *ht);
 
 
 #endif
